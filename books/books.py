8382a02d3dd8446fd08b94503daba029afcd9e13
Sophia <wangs7@carleton.edu>
2022-09-29 08:39:24 -0500

    allowed user to input file

--- books/books.py
+++ books/books.py
@@ -8,45 +8,52 @@
     CS 257 Software Design class, Fall 2022.
 '''
 
+'''
+user puts in: python3 books.py <file> -t searchstring sortby
+'''
+
 import csv
 import sys
 import booksdatasource
 
 
 def main():
-    books_file = open('books1.csv')
+    if len(sys.argv) < 2:
+        #raise an error: too few arguments
+        pass
+    books_file = open(sys.argv[1])
     dataSource = booksdatasource.BooksDataSource(books_file)
-    if len(sys.argv) == 1:
+    if len(sys.argv) == 2:
         output = dataSource.books()
-    elif sys.argv[1] == "-h" or sys.argv[1] == "--help":
+    elif sys.argv[2] == "-h" or sys.argv[2] == "--help":
         usage = open('usage.txt')
         print(usage.read())
         usage.close()
-    elif sys.argv[1] == "-t" or sys.argv[1] == "--title":
-        if len(sys.argv) == 2:
+    elif sys.argv[2] == "-t" or sys.argv[2] == "--title":
+        if len(sys.argv) == 3:
             output = dataSource.books()
-        elif len(sys.argv) == 3:
-            output = dataSource.books(sys.argv[2])
         elif len(sys.argv) == 4:
-            output = dataSource.books(sys.argv[2], sys.argv[3])
+            output = dataSource.books(sys.argv[3])
+        elif len(sys.argv) == 5:
+            output = dataSource.books(sys.argv[3], sys.argv[4])
         else:
             raise SyntaxError('Wrong number of arguments')
-    elif sys.argv[1] == "-a" or sys.argv[1] == "--author":
-        if len(sys.argv) == 2:
+    elif sys.argv[2] == "-a" or sys.argv[2] == "--author":
+        if len(sys.argv) == 3:
             output = dataSource.authors()
-        elif len(sys.argv) == 3:
-            output = dataSource.authors(sys.argv[2])
         elif len(sys.argv) == 4:
-            output = dataSource.authors(sys.argv[2], sys.argv[3])
+            output = dataSource.authors(sys.argv[3])
+        elif len(sys.argv) == 5:
+            output = dataSource.authors(sys.argv[3], sys.argv[4])
         else:
             raise SyntaxError('Wrong number of arguments')
-    elif sys.argv[1] == "-y" or sys.argv[1] == "--year":
-        if len(sys.argv) == 2:
+    elif sys.argv[2] == "-y" or sys.argv[1] == "--year":
+        if len(sys.argv) == 3:
             output = dataSource.books_between_years()
-        elif len(sys.argv) == 3:
-            output = dataSource.books_between_years(sys.argv[2])
         elif len(sys.argv) == 4:
-            output = dataSource.books_between_years(sys.argv[2], sys.argv[3])
+            output = dataSource.books_between_years(sys.argv[3])
+        elif len(sys.argv) == 5:
+            output = dataSource.books_between_years(sys.argv[3], sys.argv[4])
         else:
             raise SyntaxError('Wrong number of arguments')
     else:
